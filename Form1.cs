fcde73f558060df58ebc9a53fb855b2b1e7fc6d0
SimCodes <157691035+SimChords@users.noreply.github.com>
2026-04-28 16:37:59 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace My_Favourite_Programming_Languages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
